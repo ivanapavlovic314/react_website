3b776352d67b8bfc27eb3c5e99df5bfea2e8342b
Ivana Pavlovic <ivanapavlovic314@users.noreply.github.com>
2022-03-17 20:05:04 +0100

    Creating first Component commit, closes #3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h2>Hello World!</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My First React Project!
         </p>
+        <ExpenseItem/>
         <a
           className="App-link"
           href="https://reactjs.org"
