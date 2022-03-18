adb6555673b0f56c8eeb03fde381811979412227
Ivana Pavlovic <ivanapavlovic314@users.noreply.github.com>
2022-03-18 21:46:33 +0100

    Basic css commit, closes #5

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,11 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
 
-    return <div>
-                <div>March 28th 2022</div>
+    return <div className="expense-item">
+                <div className="expense-item__description">March 28th 2022</div>
                 <div>
-                    <h2>Car Insurance</h2>
+                    <h2 className="expense-item__price">Car Insurance</h2>
                     <p>$294.67</p>
                 </div>
             </div>;
