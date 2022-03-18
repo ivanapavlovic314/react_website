cab43572480e9ba4c0878c42787e014419515368
Ivana Pavlovic <ivanapavlovic314@users.noreply.github.com>
2022-03-18 23:43:39 +0100

    Composition concept commit, closes #10

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
+
 
 function App() {
+
   const expenses = [
     {
       id: 'e1',
@@ -26,24 +28,11 @@ function App() {
   ];
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <img src={logo} className="App-logo" alt="logo" />
 
-        <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
-        <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>
-        <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}/>
-        <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}/>
+      <Expenses items={expenses}/>
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
