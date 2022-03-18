ac03a54f5a39fd7d63233b4eb730aacb54a40ec2
Ivana Pavlovic <ivanapavlovic314@users.noreply.github.com>
2022-03-18 21:58:19 +0100

    Basic css commit, closes #6

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,11 +2,17 @@ import './ExpenseItem.css';
 
 function ExpenseItem() {
 
+    const expenseDate = new Date(2022, 2, 28);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 294.67;
+
+
+
     return <div className="expense-item">
-                <div className="expense-item__description">March 28th 2022</div>
+                <div className="expense-item__description">{expenseDate.toISOString()}</div>
                 <div>
-                    <h2 className="expense-item__price">Car Insurance</h2>
-                    <p>$294.67</p>
+                    <h2 className="expense-item__price">{expenseTitle}</h2>
+                    <p>{expenseAmount}</p>
                 </div>
             </div>;
 
